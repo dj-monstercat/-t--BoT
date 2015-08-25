996ec1973edb74a7e1f31b61c3d6a375e311a954
dj-monstercat <jordansim189189@gmail.com>
2015-08-25 15:39:45 -0500

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1847,12 +1847,13 @@
                         }
                         else {
                             trapbot.settings.filterChat = !trapbot.settings.filterChat;
-                       }    return API.sendChat(subChat(trapbot.chat.toggleon, {name: chat.un, 'function': trapbot.chat.chatfilter}));
-                   }    
-               }     
-           },  
-                  
-           gifCommand: {
+                            return API.sendChat(subChat(trapbot.chat.toggleon, {name: chat.un, 'function': trapbot.chat.chatfilter}));
+                        }
+                    }
+                } 
+            },      
+              
+            gifCommand: {
                 command: ['gif', 'giphy'],
                 rank: 'user',
                 type: 'startsWith',
